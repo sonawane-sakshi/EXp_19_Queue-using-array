2b8038db9e317d2c8b7926fd75c9aff2b1d9841c
sonawane-sakshi <h116sonawanesakshi@gmail.com>
2024-10-15 01:21:07 +0530

    Add files via upload

---
+++ Queue Using STL.cpp
@@ -0,0 +1,24 @@
+//Queue implementation ussing STL
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    queue<int> q;
+
+    // Adding elements to the queue
+q.push(30);
+q.push(20);
+q.push(10);
+
+    // Print the front element of the queue
+cout<< "Front element before pop: " <<q.front() <<endl;
+
+    // Remove the front element from the queue
+q.pop();
+
+    // Print the front element after pop
+cout<< "Front element after pop: " <<q.front() <<endl;
+
+    return 0;
+}
